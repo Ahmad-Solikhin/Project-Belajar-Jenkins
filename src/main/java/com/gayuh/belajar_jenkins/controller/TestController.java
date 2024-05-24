c54acc89b3e454a19f191f564587a173919ede41
Gayuh <ahmadsgr39@gmail.com>
2024-05-24 14:30:51 +0700

    Initial commit

---
+++ src/main/java/com/gayuh/belajar_jenkins/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.gayuh.belajar_jenkins.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<Object> helloWorld() {
+        Map<String, Object> response = Map.of(
+                "message", "Hello World",
+                "status", 200
+        );
+
+        return ResponseEntity.ok(response);
+    }
+}
